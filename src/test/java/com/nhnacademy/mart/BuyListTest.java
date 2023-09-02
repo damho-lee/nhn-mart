36ade119f531fadc48256d3bcff2fe32e541fbf3
{damho-lee} <{ydh4745@gmail.com}>
2023-09-02 13:30:56 +0900

    BuyListTest.java 작성

--- src/test/java/com/nhnacademy/mart/BuyListTest.java
+++ src/test/java/com/nhnacademy/mart/BuyListTest.java
@@ -1,13 +1,36 @@
 package com.nhnacademy.mart;
 
+import com.nhnacademy.mart.BuyList.Item;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class BuyListTest {
+    private static Stream<Arguments> buyListSource() {
+        return Stream.of(
+                Arguments.of("사과", 2),
+                Arguments.of("오렌지", 5),
+                Arguments.of("스윙칩", 1)
+        );
+    }
+
     @Test
-    @DisplayName("Item Constructor Test (amount < 0)")
+    @DisplayName("Item Constructor Test (amount < 1)")
     public void itemConstructorTest() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new BuyList.Item("사과", 0));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Item("사과", 0));
+    }
+
+    @ParameterizedTest
+    @MethodSource("buyListSource")
+    @DisplayName("BuyList add Method Test")
+    public void buyListAddMethodTest(String name, int amount) {
+        BuyList buyList = new BuyList();
+        buyList.add(name, amount);
+        Assertions.assertEquals(name, buyList.getItems().get(0).getName());
+        Assertions.assertEquals(amount, buyList.getItems().get(0).getAmount());
     }
 }
